54cc7812a3fe241faca1504abd758279a2dddfcb
Dima <88742126+DimaWeb44@users.noreply.github.com>
2022-03-20 14:49:28 +0300

    Create lesson4.js

---
+++ lesson4.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+let a = fixedValue
+return a + 1
+  // Only change code above this line
+}
